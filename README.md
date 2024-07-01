90d2f890c525581e8f75275ae00da83db5cece29
vitthalk15 <136836610+vitthalk15@users.noreply.github.com>
2024-07-01 13:51:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio_web
+# Portfolio_web
---
+++ script.js
@@ -0,0 +1,93 @@
+function myMenuFunction(){
+    var menuBtn = document.getElementById('my-nav-menu')
+
+    if(menuBtn.className == "nav-menu"){
+        menuBtn.className += " responsive"
+    }else{
+        menuBtn.className = "nav-menu"
+    }
+}
+
+
+// Dark Mode
+const body = document.querySelector("body"),
+    toggleSwitch = document.getElementById("toggle-switch")
+
+toggleSwitch.addEventListener("click",()=>{
+    body.classList.toggle("dark")
+});
+
+
+// typing effect
+var typingEffect = new Typed(".typedText",{
+    strings:['CODER','DEVELOPER','DESIGNER','VITTHAL'],
+
+    loop:true,
+    typeSpeed:100,
+    backSpeed:80,
+    backDelay:2000,
+})
+
+// scrolling animation
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'80px',
+    duration:2000,
+    reset:true,
+});
+
+sr.reveal(".featured-name",{delay:100})
+sr.reveal(".text-info",{delay:200})
+sr.reveal(".text-btn",{delay:200})
+sr.reveal(".social-icons",{delay:200})
+sr.reveal(".featured-image",{delay:320})
+
+sr.reveal(".project-box",{interval:200})
+
+sr.reveal(".top-header",{})
+
+const srLeft = ScrollReveal({
+    origin:'left',
+    distance:'80px',
+    duration:2000,
+    reset:true,
+})
+
+srLeft.reveal(".about-info",{delay:100})
+srLeft.reveal(".contact-info",{delay:100})
+
+const srRight = ScrollReveal({
+    origin:'right',
+    distance:'80px',
+    duration:2000,
+    reset:true
+})
+
+srRight.reveal(".skill",{delay:100})
+srRight.reveal(".skill-box",{delay:100})
+
+
+// Active link
+const sections = document.querySelectorAll('.section[id]')
+
+function scrollActive(){
+    const scrollY = window.scrollY
+
+    sections.forEach((current)=>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document
+            .querySelector(".nav-menu a[href*=" + sectionId + "]" )
+            .classList.add('active-link')
+        }else{
+            document
+            .querySelector(".nav-menu a[href*=" + sectionId + "]" )
+            .classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener("scroll",scrollActive)  
